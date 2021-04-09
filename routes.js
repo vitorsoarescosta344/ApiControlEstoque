552f5ba4bbe2d831f150652cc5c281baac0ad2ff
victor <70414722+vitorsoarescosta344@users.noreply.github.com>
2021-04-09 20:21:55 -0300

    CORS

--- routes.js
+++ routes.js
@@ -6,6 +6,7 @@ app.use(cors())
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE')
     next()
 })
 
@@ -16,6 +17,6 @@ app.get("/:token/produto", cors() , ProdutoController.index);
 
 app.post("/:token/produto", cors(), ProdutoController.store);
 
-routes.get("/:token/produto/:codDeBarras", ProdutoController.getByCod)
+app.get("/:token/produto/:codDeBarras", ProdutoController.getByCod)
 
 module.exports = routes;
