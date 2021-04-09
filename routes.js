8a737becd17dd6f47770ac85011d083092839e1e
victor <70414722+vitorsoarescosta344@users.noreply.github.com>
2021-04-09 20:52:10 -0300

    CORS

--- routes.js
+++ routes.js
@@ -3,12 +3,12 @@ const cors = require('cors')
 
 const app = express()
 
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE')
     app.use(cors())
     next()
-})
+})*/
 
 //app.use(cors())
 
