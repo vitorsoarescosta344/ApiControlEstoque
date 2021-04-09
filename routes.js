3e80a7818e256bd53d1f970753deb8c80b165b16
victor <70414722+vitorsoarescosta344@users.noreply.github.com>
2021-04-09 19:03:38 -0300

    CORS

--- routes.js
+++ routes.js
@@ -2,19 +2,19 @@ const express = require("express");
 const cors = require('cors')
 
 const app = express()
+app.use(cors())
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*")
-    app.use(cors())
     next()
 })
 
 const routes = express.Router();
 const ProdutoController = require("./app/controller/ProdutoController");
 
-routes.get("/:token/produto", ProdutoController.index);
+routes.get("/:token/produto", cors() , ProdutoController.index);
 
-routes.post("/:token/produto", ProdutoController.store);
+routes.post("/:token/produto", cors(), ProdutoController.store);
 
 routes.get("/:token/produto/:codDeBarras", ProdutoController.getByCod)
 
