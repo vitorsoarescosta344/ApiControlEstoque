a04ef095abbc750a55e123b7dd7c7b502e995a4a
victor <70414722+vitorsoarescosta344@users.noreply.github.com>
2021-04-09 21:49:08 -0300

    CORS

--- routes.js
+++ routes.js
@@ -13,8 +13,8 @@ const app = express()
 
 
 var corsOptions = {
-    "origin": "*",
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     optionsSuccessStatus: 200
 }
 
