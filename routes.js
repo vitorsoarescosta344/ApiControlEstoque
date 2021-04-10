a2e56bbab506342da1f5019acded41b4ff3c24b5
victor <70414722+vitorsoarescosta344@users.noreply.github.com>
2021-04-10 07:18:14 -0300

    CORS

--- routes.js
+++ routes.js
@@ -23,7 +23,7 @@ var corsOptions = {
 const routes = express.Router();
 
 routes.use((req, res, next) =>{
-    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
     next()
 })
 const ProdutoController = require("./app/controller/ProdutoController");
