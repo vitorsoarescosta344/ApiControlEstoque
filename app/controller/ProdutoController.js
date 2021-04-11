fb0d73f898eade97638ac5395f44bcaee5f54c82
victor <70414722+vitorsoarescosta344@users.noreply.github.com>
2021-04-11 20:07:20 -0300

    Teste

--- app/controller/ProdutoController.js
+++ app/controller/ProdutoController.js
@@ -3,8 +3,14 @@ const Produto = require("../model/Produto");
 class ProdutoController {
   async store(req, res) {
     if(req.params.token === '12345678'){
-        const data = await Produto.create(req.body);
-        return res.json(data);       
+
+      let IsValid = Produto.findOne({codDeBarras: res.body.codDeBarras}, (err, res)=>{
+        if(res && res.codDeBarras === res.codDeBarras){
+          console.log({error: "Produto já existe"})
+        }
+      })
+      const data = await Produto.create(req.body);
+      return res.json(data);       
     }
 
     return res.status(500).json({ error: "falha na autenticação" })
