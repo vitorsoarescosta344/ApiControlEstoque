0176106e14b7cd37da85b649739600a90e2ff386
victor <70414722+vitorsoarescosta344@users.noreply.github.com>
2021-04-28 08:54:15 -0300

    Allow all origins

--- routes.js
+++ routes.js
@@ -23,7 +23,7 @@ var corsOptions = {
 const routes = express.Router();
 
 routes.use((req, res, next) =>{
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+    res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Headers', '*')
     res.header('Access-Control-Allow-Methods', '*')
     next()
